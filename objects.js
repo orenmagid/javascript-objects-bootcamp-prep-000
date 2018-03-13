14e9e02036391e78de4a985f5cbd332c71d0e7bf
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 08:46:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 playlist = {Nirvana: "Smells Like Teen Spirit"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
 
